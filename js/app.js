5ce37daf2396ddafa6a45fb4476ac1b4710d6dcc
eyawson <ebow.yawson@gmail.com>
2019-04-06 16:41:02 -0400

    added service worker file and more vue data

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const app = new Vue({
+    el: '#ey',
+    data: {
+        name: 'Ebow Yawson',
+        visitorName: '',
+        bio: '',
+        majorSkills: ['Html', 'CSS', 'JavaScript', 'Accessibility', 'PWA'],
+        minorSkills: ['React', 'Vue']
+    }
+})
