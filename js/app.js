dae09b408e9fa6da4e302eba4c2c5838b1baf5f5
eyawson <ebow.yawson@gmail.com>
2019-12-31 22:46:52 -0500

    fix: updated content
    added more info to bio and merged old portfolio code

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 const app = new Vue({
     el: '#ey',
     data: {
-        name: 'Ebow Yawson',
-        visitorName: '',
-        bio: '',
-        majorSkills: ['Html', 'CSS', 'JavaScript', 'Accessibility', 'PWA'],
-        minorSkills: ['React', 'Vue']
+        intro: 'Welcome to my page. My name is Ebow Yawson and I am glad you checked me out. Hopefully, you will hire me or point me in the right direction.',
+        //visitorName: '',
+        bio: 'I am a cloud developer here looking to be a part of your team. I have years of experience working professionaly in different roles. I have been working very hard at web and cloud development and I am ready to be part of your team and contribute meaningfuly. This is a love, and will not change whether it is my full-time job (if you hire me) or a hobby.',
+        majorSkills: ['Html', 'CSS', 'JavaScript', 'Cloud Development', 'AWS', 'Microservices', 'Docker', 'Kubernetes', 'SQL', 'Accessibility', 'PWA'],
+        minorSkills: ['React', 'Vue', 'IONIC', 'Lambda'],
+        friends: 'If i do no fit your role, how about you check out my friends. There are gems all over here',
+        contact: ['973 489 2670', 'ebow.yawson@gmail.com']
     }
 })
