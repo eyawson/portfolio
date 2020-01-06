2a381fc4f7e940c19e57e785042c7c6ff3e16ea3
eyawson <ebow.yawson@gmail.com>
2020-01-05 23:18:17 -0500

    fix: added background color
    Added background colors to address and footer sections

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const app = new Vue({
         bio: 'I am a cloud developer here looking to be a part of your team. I have years of experience working professionaly in different roles. I have been working very hard at web and cloud development and I am ready to be part of your team and contribute meaningfuly. This is a love, and will not change whether it is my full-time job (if you hire me) or a hobby.',
         majorSkills: ['Html', 'CSS', 'JavaScript', 'Cloud Development', 'AWS', 'Microservices', 'Docker', 'Kubernetes', 'SQL', 'Accessibility', 'PWA'],
         minorSkills: ['React', 'Vue', 'IONIC', 'Lambda'],
-        friends: 'If i do no fit your role, how about you check out my friends. There are gems all over here',
+        friends: 'If i do no fit your role, how about you check out my friends. There are gems all over ',
         contact: ['973 489 2670', 'ebow.yawson@gmail.com']
     }
 })
