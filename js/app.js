679028ec6e5e20b724cf1582c3e4d351f5018af6
eyawson <ebow.yawson@gmail.com>
2020-01-21 11:32:46 -0500

    style: switch images
    removed placeholder images for protfolio and added screenshots or actual work

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 const app = new Vue({
   el: "#ey",
   data: {
-    intro:
-      "Welcome to my page. My name is Ebow Yawson and I am glad you checked me out. Hopefully, we can have a conversation",
+    intro: "Welcome to my page. My name is Ebow Yawson and I am glad you checked me out. Hopefully, we can have a conversation",
     //visitorName: '',
-    bio:
-      "I am a full-stack cloud developer looking to be a part of your team. In my many years of working professionally, I have risen rapidly through the rank due to hard work, commitment, and seeing through tasks. I have been working very hard at this career change to web and cloud development. I believe I am ready to be part of your team and contribute meaningfully. This is a love to build and figure things out.",
+    bio: "I am a full-stack cloud developer looking to be a part of your team. In my many years of working professionally, I have risen rapidly through the rank due to hard work, commitment, and seeing through tasks. I have been working very hard at this career change to web and cloud development. I believe I am ready to be part of your team and contribute meaningfully. This is a love to build and figure things out.",
     majorSkills: [
       "Html",
       "CSS",
@@ -25,24 +23,23 @@ const app = new Vue({
     ],
     friends: "If I do no fit your role, how about you check out my friends on ",
     contact: ["973 489 2670", "ebow.yawson@gmail.com"],
-    showcase: [
-      {
+    showcase: [{
         name: "Udagram Microservice",
-        image: "/images/cloud3.jpg",
+        image: "/images/udagram.png",
         alt: "image repo",
         skills: ["Kubernetes", "Docker", "Travis CI"],
         link: "https://github.com/eyawson/udacity-microservice-project"
       },
       {
         name: "Phebe's book",
-        image: "./images/cloud2.jpg",
+        image: "./images/phebe.png",
         alt: "image repo",
         skills: ["HTML", "CSS", "Javascript"],
         link: "https://github.com/eyawson/phebebook"
       },
       {
         name: "Ravenous a Yelp clone",
-        image: "/images/cloud1.jpg",
+        image: "/images/qli.png",
         alt: "image repo",
         skills: ["React", "APIs", "CRUD"],
         link: "https://github.com/eyawson/ravenous"
