1807132e0997a2f94e88db7cb9d31ed09e51a830
eyawson <ebow.yawson@gmail.com>
2020-01-28 23:17:55 -0500

    fix: reworded vocabulary in summary text

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const app = new Vue({
     name: ' Hello!',
     intro: 'I\'m Ebow',
     introB: 'Web Developer "+" Problem Solver',
-    bio: 'I am a full-stack cloud developer looking to be a part of your team. In my many years of working professionally, I have risen rapidly through the rank due to hard work, commitment, and seeing through tasks. I have been working very hard at this career change to web and cloud development. I believe I am ready to be part of your team and contribute meaningfully. This is a love to build and figure things out. Download my ',
+    bio: 'I am a full-stack cloud developer looking to be a part of your team. In my many years of working professionally, I have risen rapidly through the rank due to hard work, commitment, and seeing tasks through to the end. I have been working very hard at this career change to web and cloud development. I believe I am ready to be part of your team and contribute meaningfully. This comes from a love to build and figure things out. Download my ',
     majorSkills: [
       'Html',
       'CSS',
